36a2a6d5a152d044acecd3f5f42dea9a51fc45f5
Luís Felipe Souza <66641151+lfwdev@users.noreply.github.com>
2025-02-20 18:46:28 -0300

    Create dokploy-local.sh

---
+++ docker/dokploy-local.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+install_dokploy() {
+    if [ "$(id -u)" != "0" ]; then
+        echo "This script must be run as root" >&2
+        exit 1
+    fi
+
+    # check if is Mac OS
+    if [ "$(uname)" = "Darwin" ]; then
+        echo "This script must be run on Linux" >&2
+        exit 1
+    fi
+
+    # check if is running inside a container
+    if [ -f /.dockerenv ]; then
+        echo "This script must be run on Linux" >&2
+        exit 1
+    fi
+
+    # check if something is running on port 80
+    if ss -tulnp | grep ':80 ' >/dev/null; then
+        echo "Error: something is already running on port 80" >&2
+        exit 1
+    fi
+
+    # check if something is running on port 443
+    if ss -tulnp | grep ':443 ' >/dev/null; then
+        echo "Error: something is already running on port 443" >&2
+        exit 1
+    fi
+
+    command_exists() {
+      command -v "$@" > /dev/null 2>&1
+    }
+
+    if command_exists docker; then
+      echo "Docker already installed"
+    else
+      curl -sSL https://get.docker.com | sh
+    fi
+
+    docker swarm leave --force 2>/dev/null
+
+    # Get the IP address from the argument or use localhost as default
+    advertise_addr="${1:-localhost}"
+    echo "Using advertise address: $advertise_addr"
+
+    docker swarm init
+    
+     if [ $? -ne 0 ]; then
+        echo "Error: Failed to initialize Docker Swarm" >&2
+        exit 1
+    fi
+
+    echo "Swarm initialized"
+
+    docker network rm -f dokploy-network 2>/dev/null
+    docker network create --driver overlay --attachable dokploy-network
+
+    echo "Network created"
+
+    mkdir -p /etc/dokploy
+
+    chmod 777 /etc/dokploy
+
+    docker pull postgres:16
+    docker pull redis:7
+    docker pull traefik:v3.1.2
+    docker pull dokploy/dokploy:latest
+
+    # Installation
+    docker service create \
+      --name dokploy \
+      --replicas 1 \
+      --network dokploy-network \
+      --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+      --mount type=bind,source=/etc/dokploy,target=/etc/dokploy \
+      --mount type=volume,source=dokploy-docker-config,target=/root/.docker \
+      --publish published=3000,target=3000,mode=host \
+      --update-parallelism 1 \
+      --update-order stop-first \
+      --constraint 'node.role == manager' \
+      -e ADVERTISE_ADDR=$advertise_addr \
+      dokploy/dokploy:latest
+
+    GREEN="\033[0;32m"
+    YELLOW="\033[1;33m"
+    BLUE="\033[0;34m"
+    NC="\033[0m" # No Color
+
+    format_ip_for_url() {
+        local ip="$1"
+        if echo "$ip" | grep -q ':'; then
+            # IPv6
+            echo "[${ip}]"
+        else
+            # IPv4
+            echo "${ip}"
+        fi
+    }
+
+    formatted_addr=$(format_ip_for_url "$advertise_addr")
+    echo ""
+    printf "${GREEN}Congratulations, Dokploy is installed!${NC}\n"
+    printf "${BLUE}Wait 15 seconds for the server to start${NC}\n"
+    printf "${YELLOW}Please go to http://${formatted_addr}:3000${NC}\n\n"
+}
+
+update_dokploy() {
+    echo "Updating Dokploy..."
+    
+    # Pull the latest image
+    docker pull dokploy/dokploy:latest
+
+    # Update the service
+    docker service update --image dokploy/dokploy:latest dokploy
+
+    echo "Dokploy has been updated to the latest version."
+}
+
+# Main script execution
+if [ "$1" = "update" ]; then
+    update_dokploy
+else
+    install_dokploy
+fi
